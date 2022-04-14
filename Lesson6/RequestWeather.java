ed2b004047238ab929b51511d4a008d4660a87d2
WildAsphodel <99406856+WildAsphodel@users.noreply.github.com>
2022-04-14 19:07:29 +0300

    Create RequestWeather

---
+++ Lesson6/RequestWeather.java
@@ -0,0 +1,37 @@
+package Lesson6;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class RequestWeather {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("295212")
+                .addQueryParameter("apikey", "LybOis5wipwMUhYawuTDZ7j3uQHyKyKx")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+
+        System.out.println(response.code());
+        String body = response.body().string();
+        System.out.println(body);
+    }
+}
